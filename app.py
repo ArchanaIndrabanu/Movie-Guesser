801e82f7b93182d1888c6b705b8db35ef70f695f
ArchanaIndrabanu <archanaindrabanu_bai26@mepcoeng.ac.in>
2024-08-18 18:28:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import torch
+import requests
 from sentence_transformers import SentenceTransformer, util
 
 # Page Config
@@ -46,19 +47,23 @@ model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
 plots = subset_df['Plot'].tolist()
 plot_embeddings = model.encode(plots, convert_to_tensor=True)
 
-# Define the movie finding function
+# Function to find the most similar movie
 def find_movie(description):
-    # Encode the description
     description_embedding = model.encode(description, convert_to_tensor=True)
-    
-    # Compute cosine similarities
     cosine_scores = util.pytorch_cos_sim(description_embedding, plot_embeddings)[0]
-    
-    # Find the index of the highest cosine similarity
     top_result = torch.argmax(cosine_scores).item()
-    
-    # Return the title of the most similar movie
     return subset_df.iloc[top_result]['Title']
+
+# Function to fetch movie poster
+def fetch_movie_poster(movie_title):
+    api_key = "dfc64c2f"  # Replace with your OMDb API key
+    url = f"http://www.omdbapi.com/?t={movie_title}&apikey={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    if 'Poster' in data and data['Poster'] != 'N/A':
+        return data['Poster']
+    else:
+        return None
 
 # Streamlit App Input and Output
 st.markdown('<p class="medium-font">Find Your Movie 🎥</p>', unsafe_allow_html=True)
@@ -68,6 +73,13 @@ if description:
     st.markdown('<p class="medium-font">🔍 Searching for the most similar movie...</p>', unsafe_allow_html=True)
     movie_title = find_movie(description)
     st.markdown(f'<p class="medium-font">**The most similar movie is: _{movie_title}_**</p>', unsafe_allow_html=True)
+
+    # Fetch and display the movie poster
+    poster_url = fetch_movie_poster(movie_title)
+    if poster_url:
+        st.image(poster_url, caption=movie_title, use_column_width=True)
+    else:
+        st.write("**Poster not available**")
 
     # Optionally, display more details about the movie
     movie_details = subset_df[subset_df['Title'] == movie_title].iloc[0]
@@ -80,7 +92,8 @@ else:
 # Footer
 st.divider()
 st.markdown("""
-    <p class="small-font">  
+    <p class="small-font">
+    **App Developed by [Your Name]**  
     _Powered by Streamlit and Sentence Transformers_
     </p>
     """, unsafe_allow_html=True)
